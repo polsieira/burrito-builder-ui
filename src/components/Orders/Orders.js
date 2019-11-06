3d1affb1955eda59fef7982cbb0f48b4ac8f5634
posi7790 <posi7790@colorado.edu>
2019-11-06 09:37:01 -0700

    Make App functional component and move fetch and redux to orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Orders.css';
+import { getOrders } from '../../apiCalls';
+import Order from '../Order/Order';
+import { connect } from 'react-redux';
+import { setOrders } from '../../actions';
+import { bindActionCreators } from 'redux';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+
+export class Orders extends Component {
+
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+  render() {
+    const { orders } = this.props;
+    const orderEls = orders.map(order => {
+      return (
+        <Order order={order} key={order.id} />
+      )
+    });
     return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
+      <section>
+        {orderEls.length ? orderEls : <p>No orders yet!</p>}
+      </section >
     )
-  });
-
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  }
 }
 
-export default Orders;
+export const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+export const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    setOrders
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);

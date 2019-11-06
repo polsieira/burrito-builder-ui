7259b59340d3d5bc2658a3a03a83877e92b38805
posi7790 <posi7790@colorado.edu>
2019-11-06 09:35:45 -0700

    Add reducer for adding order

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -2,6 +2,9 @@ export const orders = (state = [], action) => {
   switch (action.type) {
     case 'SET_ORDERS':
       return action.orders;
+    case 'ADD_ORDERS':
+      console.log([...state, action.newOrder])
+      return [...state, action.newOrder];
     default:
       return state;
   }
